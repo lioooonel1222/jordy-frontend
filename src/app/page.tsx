b223941455ac0878eb573ad90071c31c43411fd8
lioooonel1222 <briellionel@gmail.com>
2025-09-14 19:10:12 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import JordyChatbot from "./JordyChatbot";
 
 export default function Home() {
-  return <JordyChatbot />;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <JordyChatbot />
+    </main>
+  );
 }
